ee57e4b4fe57f77ef3bef4d32f1360f885cfeaf5
unknown <halniyazovt98@gmail.com>
2024-02-29 15:33:02 +0300

    init

--- src/router.js
+++ src/router.js
@@ -1,29 +1,8 @@
-// import { createRouter, createWebHistory } from 'vue-router';
-// import Todo from './components/Todo.vue';
-
-// const routes = [
-//   { path: '/todo', component: Todo }
-// ];
-
-// const router = createRouter({
-//   history: createWebHistory(),
-//   routes
-// });
-
-// export default router;
 import { createRouter, createWebHistory } from 'vue-router';
 import Todo from './components/Todo.vue';
-import Main from './components/Main.vue';
 
 const routes = [
-  { 
-    path: '/vuejs-todo-app', 
-    component: Main
-   },
-   {
-    path: '/todo',
-    component: Todo
-   }
+  { path: '/todo', component: Todo }
 ];
 
 const router = createRouter({
