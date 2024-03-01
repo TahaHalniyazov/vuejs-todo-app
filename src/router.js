dc3153c50fac94962a7bf395354e4b6d564f3969
unknown <halniyazovt98@gmail.com>
2024-03-01 12:30:46 +0300

    newcommit2

--- src/router.js
+++ src/router.js
@@ -1,20 +1,45 @@
-import { createRouter, createWebHistory } from 'vue-router';
+// import { createRouter, createWebHistory } from 'vue-router';
 
+
+// const routes = [
+//   {
+//     path: '/',
+//     component: () => import('./components/Main.vue')
+//   },
+
+//   { 
+//     path: '/todo', 
+//     component: () => import('./components/Todo.vue')
+//   }
+// ];
+
+// const router = createRouter({
+//   history: createWebHistory('/vuejs-todo-app/'),
+//   routes
+// });
+
+// export default router;
+
+
+import { createRouter, createWebHashHistory } from 'vue-router';
+import Main from './components/Main.vue';
+import Todo from './components/Todo.vue';
 
 const routes = [
   {
     path: '/',
-    component: () => import('./components/Main.vue')
+    name: 'Main',
+    component: Main
   },
-
-  { 
-    path: '/todo', 
-    component: () => import('./components/Todo.vue')
+  {
+    path: '/todo',
+    name: 'Todo',
+    component: Todo
   }
 ];
 
 const router = createRouter({
-  history: createWebHistory('/vuejs-todo-app/'),
+  history: createWebHashHistory(),
   routes
 });
 
