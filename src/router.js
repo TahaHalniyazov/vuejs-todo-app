14787b8200cbb62dc1592d2cc2882b10e4397cc1
unknown <halniyazovt98@gmail.com>
2024-03-01 12:38:47 +0300

    corrected errors in router.js

--- src/router.js
+++ src/router.js
@@ -1,25 +1,3 @@
-// import { createRouter, createWebHistory } from 'vue-router';
-
-
-// const routes = [
-//   {
-//     path: '/',
-//     component: () => import('./components/Main.vue')
-//   },
-
-//   { 
-//     path: '/todo', 
-//     component: () => import('./components/Todo.vue')
-//   }
-// ];
-
-// const router = createRouter({
-//   history: createWebHistory('/vuejs-todo-app/'),
-//   routes
-// });
-
-// export default router;
-
 
 import { createRouter, createWebHashHistory } from 'vue-router';
 import Main from './components/Main.vue';
